c0fd876449f153a4b3cb2177749c6bac2e7c2886
victoriaTrabW <victoria.tw@hotmail.com>
2023-05-30 14:05:34 +0200

    figs

--- src/main.py
+++ src/main.py
@@ -41,3 +41,44 @@ results_all = pd.DataFrame({'headline': dataset_all['title'], 'emotion_huggingfa
 results_real.to_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/results_real.csv', index=False)
 results_fake.to_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/results_fake.csv', index=False)
 results_all.to_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/results_all.csv', index=False)
+
+# Now i want to count and visualise the distribution of emotions in each of the datasets
+
+# Counting  the occurrences of each emotion for each dataset
+emotion_counts_real = results_real['emotion_huggingface'].value_counts()
+emotion_counts_fake = results_fake['emotion_huggingface'].value_counts()
+emotion_counts_all = results_all['emotion_huggingface'].value_counts()
+
+# Plotting the emotion distribution for each dataset
+sns.set(style="darkgrid")
+plt.figure(figsize=(20, 8))
+
+# real headlines dataset
+plt.subplot(1, 3, 1)
+sns.barplot(x=emotion_counts_real.index, y=emotion_counts_real.values)
+plt.xlabel("Emotion")
+plt.ylabel("Count")
+plt.title("Emotion Distribution for REAL Headlines")
+plt.ylim(0, 1700)  # Setting the same maximum count for both subplots to easier compare
+
+# fake headlines dataset
+plt.subplot(1, 3, 2)
+sns.barplot(x=emotion_counts_fake.index, y=emotion_counts_fake.values)
+plt.xlabel("Emotion")
+plt.ylabel("Count")
+plt.title("Emotion Distribution for FAKE Headlines")
+plt.ylim(0, 1700)  # Setting the same maximum count for both subplots to easier compare
+
+#saving only the fake and real visualisation in out folder
+plt.savefig('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/emotion_distribution_fake_real.png')
+
+plt.subplot(1, 3, 3)
+sns.barplot(x=emotion_counts_all.index, y=emotion_counts_all.values)
+plt.xlabel("Emotion")
+plt.ylabel("Count")
+plt.title("Emotion Distribution for All Headlines")
+
+plt.tight_layout(pad=2.0) 
+
+# saving all the plots side by side in out folder
+plt.savefig('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/emotion_distribution_all_datasets.png')
