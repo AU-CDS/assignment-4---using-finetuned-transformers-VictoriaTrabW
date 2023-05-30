14d1b310c205cce50b730a321d5c12ecafecba14
victoriaTrabW <victoria.tw@hotmail.com>
2023-05-30 12:47:20 +0200

    ch

--- src/main.py
+++ src/main.py
@@ -1,19 +1,43 @@
-# imports
 import pandas as pd
 from transformers import pipeline
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # loading the dataset
-dataset = pd.read_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/in/fake_or_real_news.csv')
-
+dataset_all = pd.read_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/in/fake_or_real_news.csv')
+# Filtering the dataset for headlines with label "REAL"
+dataset_real = dataset_all[dataset_all['label'] == 'REAL']
+# Filter the dataset for headlines with label "FAKE"
+dataset_fake = dataset_all[dataset_all['label'] == 'FAKE']
 
 # initializing a huggingface pipeline
 emotion_classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
 
-# iterating over the headlines in the dataset and appending 
-emotions = []
-for headline in dataset['title']:
-    result = emotion_classifier(headline)
-    emotion = result[0]['label']
-    emotions.append(emotion)
+# Function to classify emotions for a given dataset
+def classify_emotions(data):
+    emotions = []
+    for headline in data['title']:
+        result = emotion_classifier(headline)
+        emotion = result[0]['label']
+        emotions.append(emotion)
+    return emotions
+
+# Classify emotions for headlines with label "REAL"
+emotions_real = classify_emotions(dataset_real)
+
+# Classify emotions for headlines with label "FAKE"
+emotions_fake = classify_emotions(dataset_fake)
+
+# classifying emotions for all headlines
+emotions_all = classify_emotions(dataset_all)
+
+
+# Create separate DataFrames for the results
+results_real = pd.DataFrame({'headline': dataset_real['title'], 'emotion_huggingface': emotions_real})
+results_fake = pd.DataFrame({'headline': dataset_fake['title'], 'emotion_huggingface': emotions_fake})
+results_all = pd.DataFrame({'headline': dataset_all['title'], 'emotion_huggingface': emotions_all})
+
+# Saving the results to separate CSV files in the "out" folder
+results_real.to_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/results_real.csv', index=False)
+results_fake.to_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/results_fake.csv', index=False)
+results_all.to_csv('assignment-4---using-finetuned-transformers-VictoriaTrabW/out/results_all.csv', index=False)
